6bd38209c8a33929ac779ad3e410c61d3237c82e
王珏 <wangjue@wangjuedeMacBook-Pro.local>
2018-04-10 19:30:37 -0400

    football

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import model
+
 
 app = Flask(__name__)
 
@@ -9,13 +10,42 @@ def index():
         <img src="/static/blockM.png"/>
         <h1>Michigan Sports Info!</h1>
         <ul>
-            <li><a href="/bball"> Men's Basketball </a></li>
+            <li><a href="/football"> Men's Football </a></li>
         </ul>
     '''
 
-@app.route('/bball')
+
+@app.route('/football', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+        
+    return render_template("seasons.html", seasons=seasons)
+
+
+@app.route('/hello', methods=["Get","POST"])
+def hello():
+    if request.method == "POST":
+
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        
+        firstname = ""
+        lastname = ""
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+
+
+    
+
+
+
 
 if __name__ == '__main__':
+    model.init_bball()
+    # print(model.get_bball_seasons())
     app.run(debug=True)
